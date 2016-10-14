f434aa28a49b13dd579ebb0507d952aa8ad46445
donwo <donwo@SIRIUS>
2016-10-14 00:30:41 -0500

    Add project files.

---
+++ MVCMovie2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCMovie2.Startup))]
+namespace MVCMovie2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
